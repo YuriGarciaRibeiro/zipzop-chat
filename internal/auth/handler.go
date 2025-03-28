8f44de18c17e56050f4c3c189bf34ac02a959d4d
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2025-03-28 15:01:42 -0300

    Feat(auth): include user name in login response payload

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -64,7 +64,7 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"token": token,
-		"user":  map[string]interface{}{"id": user.ID.String(), "email": user.Email},
+		"user":  map[string]interface{}{"id": user.ID.String(), "email": user.Email, "name": user.Name},
 	})
 }
 
